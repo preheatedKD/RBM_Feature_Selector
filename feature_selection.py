aeb5cf77c1a4443f48f12fc67ced90c67fe026de
Lintang Sutawika <lintangsutawika@gmail.com>
2017-04-26 00:58:17 +0700

    Normalized dataset for unit variance and zero mean

--- feature_selection.py
+++ feature_selection.py
@@ -1,18 +1,19 @@
 import RBM
+import tensorflow as tf
 import scipy.io as sio
 import numpy as np
 
-# sess = tf.Session()
-# rbm = RBM(n_hidden=,n_visible=)
-# rbm.build_model()
+dataset = sio.loadmat("glioma1.mat")
+MA_data = dataset['data']
+MA_label = dataset['ylab']
 
-mat = sio.loadmat("glioma1.mat")
-MA_data = mat['data']
-#Need to normalize
+#Normalize for unit variance and zero mean
+MA_data = (MA_data - np.mean(MA_data))/np.std(MA_data)
+
 n_items = np.shape(MA_data)[0]
 n_feature = np.shape(MA_data)[1]
 percentage = 0.5
 n_hidden = percentage * n_feature
-rbm = RBM(n_hidden=n_hidden,n_visible=n_feature)
-rbm.build_model()
+# rbm = RBM(n_hidden=n_hidden,n_visible=n_feature)
+# rbm.build_model()
 
