3187ede245d85255361c3b41f59cf86e26e5f8b0
Lintang Sutawika <lintangsutawika@gmail.com>
2017-04-26 00:12:33 +0700

    Fixed gibb sampling

--- RBM.py
+++ RBM.py
@@ -5,12 +5,12 @@ from tensorflow.examples.tutorials.mnist import input_data
 from utils import tile_raster_images
 
 class RBM(object):
-	def __init__(self, n_hidden = 500, n_visible = 784, gibbs_sampling_steps=10, alpha=0.01):
+	def __init__(self, n_hidden = 100, n_visible = 784, gibbs_sampling_steps=10000 , alpha=0.001):
 		self.alpha = alpha
 		self.input = tf.placeholder(tf.float32, [None, n_visible])
 		self.weights = tf.Variable(tf.truncated_normal([n_visible,n_hidden], stddev=1.0), name='weights_')
-		self.v_bias = tf.Variable(tf.zeros([n_visible]), name='v_bias')
-		self.h_bias = tf.Variable(tf.zeros([n_hidden]),	name='h_bias')
+		self.v_bias = tf.Variable(tf.zeros([n_visible], dtype=tf.float32),  name='v_bias')
+		self.h_bias = tf.Variable(tf.random_uniform([n_hidden], dtype=tf.float32), name='h_bias')
 
 		self.params = [self.weights, self.h_bias, self.v_bias]
 		self.gibbs_sampling_steps = gibbs_sampling_steps
@@ -31,26 +31,25 @@ class RBM(object):
 		pre_sigmoid = tf.add(self.v_bias,tf.matmul(hid,tf.transpose(self.weights)))
 		return [pre_sigmoid, tf.sigmoid(pre_sigmoid)]
 
-	def sample_h_given_v(self, v0_sample):
-		pre_sigmoid_h0, h0_probability = self.propagate_v2h(v0_sample)
-		h0_sample = tf.contrib.distributions.Bernoulli(p=h0_probability).sample()
+	def sample_h_given_v(self, v0_probability):
+		pre_sigmoid_h0, h0_probability = self.propagate_v2h(v0_probability)
+		# h0_sample = tf.contrib.distributions.Bernoulli(p=h0_probability).sample()
+		h0_sample = tf.nn.relu(tf.sign(h0_probability - tf.random_uniform(tf.shape(h0_probability))))
 		return [pre_sigmoid_h0, h0_probability, tf.cast(h0_sample,tf.float32)]
 
-	def sample_v_given_h(self, h0_sample):
-		pre_sigmoid_v1, v1_probability = self.propagate_h2v(h0_sample)
-		v1_sample = tf.contrib.distributions.Bernoulli(p=v1_probability).sample()
+	def sample_v_given_h(self, h0_probability):
+		pre_sigmoid_v1, v1_probability = self.propagate_h2v(h0_probability)
+		# v1_sample = tf.contrib.distributions.Bernoulli(p=v1_probability).sample()
+		v1_sample = tf.nn.relu(tf.sign(v1_probability - tf.random_uniform(tf.shape(v1_probability))))		
 		return [pre_sigmoid_v1, v1_probability, tf.cast(v1_sample,tf.float32)]
 
-	def gibbs_step(self, v0_sample):
-		#Visible to Hidden
-		pre_sigmoid_h0, h0_probability, h0_sample = self.sample_h_given_v(v0_sample)
+	def gibbs_step(self, h0_probability):
 		#Hidden to Visible
-		pre_sigmoid_v1, v1_probability, v1_sample = self.sample_v_given_h(h0_sample)
-		#Visible to Hidden again
-		pre_sigmoid_h1, h1_probability, h1_sample = self.sample_h_given_v(v1_sample)
-
-		return [h0_probability, h0_sample, 
-				v1_probability, v1_sample,
+		pre_sigmoid_v1, v1_probability, v1_sample = self.sample_v_given_h(h0_probability)
+		#Visible to Hidden
+		pre_sigmoid_h1, h1_probability, h1_sample = self.sample_h_given_v(v1_probability)
+		
+		return [v1_probability, v1_sample,
 				h1_probability, h1_sample]
 
 	def gibbs_vhv(self, v0_sample):
@@ -65,29 +64,36 @@ class RBM(object):
 		return [pre_sigmoid_v1, v1_probability, v1_sample,
 				pre_sigmoid_h1, h1_probability, h1_sample]		
 
-	def build_model(self):
-		h0_probability, h0_sample, v1_probability, v1_sample, h1_probability, h1_sample = self.gibbs_step(self.input)
-		nn_input = v1_probability
-		h_sample = h0_sample
-		h_probability = h0_probability
-		
-		#Positive gradient
-		self.w_positive = tf.matmul(tf.transpose(self.input), h0_sample)
+	def gibbs_sampling(self, h0_probability):
+		v1_probability = 0.0
+		h1_probability = 0.0
 
 		for step in range(self.gibbs_sampling_steps - 1):
-			h0_probability, h0_sample, v1_probability, v1_sample,h1_probability, h1_sample = self.gibbs_step(nn_input)
-			nn_input = v1_probability
+			v1_probability, v1_sample, h1_probability, h1_sample = self.gibbs_step(h0_probability)
+			h0_probability = h1_probability
 
-		self.h_probability = h1_probability
-		#Negative gradient
-		self.w_negative = tf.matmul(tf.transpose(v1_sample), h1_sample)
+		return [v1_probability, h1_probability]
+
+	def build_model(self):
+		_, h0_probability, h0_sample = self.sample_h_given_v(self.input)
+		
+		#1 Step Gibbs Sampling
+		# _, v_probability = self.propagate_h2v(h0_probability)
+		# _, h_probability = self.propagate_v2h(v1_probability)
+
+		v1_probability, h1_probability = self.gibbs_sampling(h0_probability)
+
+		self.w_positive = tf.matmul(tf.transpose(self.input), h0_sample)		
+		self.w_negative = tf.matmul(tf.transpose(v1_probability), h1_probability)
 
 		self.update_w = self.weights.assign_add(self.alpha * (self.w_positive - self.w_negative))#/tf.to_float(tf.shape(self.input)[0]))
 		self.update_vb = self.v_bias.assign_add(self.alpha * tf.reduce_mean(self.input - v1_probability, 0))
-		self.update_hb = self.h_bias.assign_add(self.alpha * tf.reduce_mean(h_probability - h1_probability,0))
-		
+		self.update_hb = self.h_bias.assign_add(self.alpha * tf.reduce_mean(h1_probability - h0_probability,0))
 		# with tf.variable_scope('loss'):
-		self.RMSE = tf.sqrt(tf.reduce_mean(tf.square(self.input - v1_sample)))
+		_, h_sample_prob, _ = self.sample_h_given_v(self.input)
+		_, _, v_sample_prob = self.sample_v_given_h(h_sample_prob)
+
+		self.RMSE = tf.reduce_mean(tf.square(self.input - v_sample_prob))
 
 		self.updates = [self.update_w, self.update_hb, self.update_vb]
 
@@ -133,7 +139,7 @@ if __name__ == '__main__':
 	rbm = RBM()
 	rbm.build_model()
 	# rbm.mnist()
-	new_w = np.zeros([784, 500], np.float32)
+	new_w = np.zeros([784, 100], np.float32)
 	
 	for num in range(Epoch):
 		for _ in range(len(mnist.train.images)/BATCH):
@@ -141,7 +147,7 @@ if __name__ == '__main__':
 			new_w, new_hb, new_vb, ReconErr = rbm.train(batch_xs)
 			# rbm.mnist_train(batch_xs, batch_ys)
 			print("Epoch: {}, Iteration: {}, Reconstruction Error: {}".format(num,_,ReconErr))
-		image = Image.fromarray(tile_raster_images(X=new_w.T,img_shape=(28, 28),tile_shape=(25, 20),tile_spacing=(1, 1)))
+		image = Image.fromarray(tile_raster_images(X=new_w.T,img_shape=(28, 28),tile_shape=(10, 10),tile_spacing=(1, 1)))
 		image.save("rbm_{}.png".format(num))
 		# rbm.mnist_test(mnist.test.images, mnist.test.labels)
 		#Testing
---
+++ feature_selection.py
@@ -0,0 +1,8 @@
+import RBM
+import scipy.io as sio
+
+# sess = tf.Session()
+# rbm = RBM(n_hidden=,n_visible=)
+# rbm.build_model()
+
+mat = sio.loadmat("glioma1.mat")
