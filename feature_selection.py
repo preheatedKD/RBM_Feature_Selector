46deb2ba5104c265a1b848e1fac8db27c6c2ee4f
Lintang Sutawika <lintangsutawika@gmail.com>
2017-04-26 08:42:58 +0700

    minor modifications to show recon error for all features

--- feature_selection.py
+++ feature_selection.py
@@ -28,7 +28,9 @@ pre_sigmoid_h1, h1_probability, h1_sample = rbm.sample_h_given_v(batch_xs)
 pre_sigmoid_v1, v1_probability, v1_sample = rbm.sample_v_given_h(h1_probability)
 
 print("Shape of v1_sample is {}".format(np.shape(v1_sample)))
-RMSE = tf.sqrt(tf.reduce_mean(tf.square(MA_data - v_sample_prob),0)/tf.to_float(tf.shape(self.input)[0]))
+RMSE = tf.sqrt(tf.reduce_mean(tf.square(MA_data - v1_sample),0)/tf.to_float(tf.shape(MA_data)[0]))
 print("Shape of RMSE: {}".format(RMSE))
+for _ in range(n_feature):
+	print("Feature {}, ReconError:{}".format(_, RMSE[_]))
 
 
